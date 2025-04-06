e2d1f1ff74caf836d65c9997cff474574d04a32f
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-04-06 17:50:59 -0400

    Exporting CheckboxCreator

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
+export { default as ButtonWithImage } from "./src/component/ButtonWithImage";
 export { default as CalendarCreator } from "./src/component/CalendarCreator";
 export { default as CarouselCreator } from "./src/component/CarouselCreator";
+export { default as CheckboxCreator } from "./src/component/CheckboxCreator";
 export { default as CollapseCreator } from "./src/component/CollapseCreator";
 export { default as SelectCreator } from "./src/component/SelectCreator";
 export { default as StepsCreator } from "./src/component/StepsCreator";
 export { default as TabCreator } from "./src/component/TabCreator";
-export { default as ButtonWithImage } from "./src/component/ButtonWithImage";
