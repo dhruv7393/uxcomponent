a80831041df2144f7f4963eb3f07e9f8e7ebdd80
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-04-06 18:29:17 -0400

    Fixing StatisticCreator

--- src/component/StatisticCreator.tsx
+++ src/component/StatisticCreator.tsx
@@ -3,12 +3,13 @@ import { Statistic } from "antd";
 interface StatisticCreatorProps {
   title: string;
   value: string;
+  of?: string;
 }
 
-const StatisticCreator = ({ title, value }: StatisticCreatorProps) => {
+const StatisticCreator = ({ title, value, of = "" }: StatisticCreatorProps) => {
   return (
     <div style={{ textAlign: "center" }}>
-      <Statistic title={title} value={value} />
+      <Statistic title={title} value={value} suffix={of && `/ ${of}`} />
     </div>
   );
 };
--- src/documentation/StatisticCreatorDoc.tsx
+++ src/documentation/StatisticCreatorDoc.tsx
@@ -3,7 +3,7 @@ import StatisticCreator from "../component/StatisticCreator";
 const StatisticCreatorDoc = () => {
   const title = "Active Users";
   const value = "112893";
-  return <StatisticCreator title={title} value={value} />;
+  return <StatisticCreator title={title} value={value} of={"20"} />;
 };
 
 export default StatisticCreatorDoc;
