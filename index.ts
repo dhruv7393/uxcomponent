11ab67aa879260725d8430fea1ef638f12f327a0
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-04-06 15:34:23 -0400

    Adding index file

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { default as CalendarCreator } from "./src/component/CalendarCreator";
+export { default as CarouselCreator } from "./src/component/CarouselCreator";
+export { default as CollapseCreator } from "./src/component/CollapseCreator";
+export { default as SelectCreator } from "./src/component/SelectCreator";
+export { default as StepsCreator } from "./src/component/StepsCreator";
+export { default as TabCreator } from "./src/component/TabCreator";
